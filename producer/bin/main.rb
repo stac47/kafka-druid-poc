cec32628d7e5c2494b036a2cb907168c0eb0ee52
Laurent Stacul <laurent.stacul@gmail.com>
2022-09-19 18:13:15 +0200

    Multi records generation

--- producer/bin/main.rb
+++ producer/bin/main.rb
@@ -6,7 +6,7 @@ require "uuid"
 require "dry-types"
 require "dry-struct"
 
-logger = Logger.new($stderr)
+$logger = Logger.new($stderr)
 
 CHECK_RESULTS = {
   clear: 1,
@@ -30,37 +30,44 @@ CHECKS_TOPIC = "check-topic"
 
 kafka = Kafka.new("localhost:9093",
                   client_id: "my-application",
-                  logger: logger)
+                  logger: $logger)
 
 kafka.create_topic(CHECKS_TOPIC) unless kafka.has_topic?(CHECKS_TOPIC)
 
-clients = (1..3).inject([]) do |memo|
-  memo << UUID.generate
-end
-
-clients.each.with_index do |client, idx|
-  logger.info("Client #{idx}: #{client}")
-end
-
 producer = kafka.producer
 
-begin
-  event = CheckEvent.new(
-    timestamp: Time.now.to_i,
-    check_uuid: UUID.generate,
-    client_uuid: clients.sample,
-    tat_ms: 1000 * Random.rand(50),
-    result: CHECK_RESULTS[CHECK_RESULTS.keys.sample]
-  )
+class Client
+  def initialize(uuid, producer)
+    @uuid = uuid
+    @producer = producer
+  end
 
-  event_json = JSON.dump(event.to_h)
-  logger.info("Event to send to Kafka: #{event_json}")
+  def send_message
+    event = CheckEvent.new(
+      timestamp: Time.now.to_i,
+      check_uuid: UUID.generate,
+      client_uuid: @uuid,
+      tat_ms: 1000 * Random.rand(50),
+      result: CHECK_RESULTS[CHECK_RESULTS.keys.sample]
+    )
 
-  producer.produce(event_json, topic: CHECKS_TOPIC)
-  producer.deliver_messages
+    event_json = JSON.dump(event.to_h)
+    $logger.info("Event to send to Kafka: #{event_json}")
+
+    @producer.produce(event_json, topic: CHECKS_TOPIC)
+  end
+end
+
+clients = (1..3).inject([]) do |memo|
+  memo << Client.new(UUID.generate, producer)
+end
+
+5.times do
+  client = clients.sample
+  client.send_message
 ensure
   producer.deliver_messages
   producer.shutdown
 
-  logger.info("Kafka producer closed")
+  $logger.info("Kafka producer closed")
 end
